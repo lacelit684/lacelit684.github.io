af882139d56dfb88e0e8086cf56a9dbdc4982d42
Some Boi <lacelit684@gmail.com>
2024-12-29 16:24:12 +0900

    i dunno why but production is the best test site rn

--- assets/scripts/contentswap.js
+++ assets/scripts/contentswap.js
@@ -1,15 +1,19 @@
 function swapContent(page) {
     const contentDiv = document.getElementById("content");
     const xhr = new XMLHttpRequest();
-    console.log(`Current working directory: ${window.location.pathname}`);
-    console.log(`Current working parent directory: ${window.location.pathname.split("/").slice(0, -1).join("/")}`);
-    xhr.open("GET", `/pages/txtswap/${page}.txt`, true);
+    xhr.open("GET", `/pages/txtswap/${page}.txt`);
     xhr.onreadystatechange = function() {
+        console.log(xhr.readyState, xhr.status);
         if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
             contentDiv.innerHTML = xhr.responseText;
         }
     };
     xhr.send();
 }
 
-swapContent("home");
+function test(sigma) {
+    console.log("Test", sigma);
+}
+
+// swapContent("home");
