62ab0b1d6c8c2169219851599da1dd6f30d707f7
Some Boi <lacelit684@gmail.com>
2024-12-29 15:58:13 +0900

    bruh

--- assets/scripts/contentswap.js
+++ assets/scripts/contentswap.js
@@ -2,6 +2,7 @@ function swapContent(page) {
     const contentDiv = document.getElementById("content");
     const xhr = new XMLHttpRequest();
     console.log(`Current working directory: ${window.location.pathname}`);
+    console.log(`Current working parent directory: ${window.location.pathname.split("/").slice(0, -1).join("/")}`);
     xhr.open("GET", `/pages/txtswap/${page}.txt`, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
